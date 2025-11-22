aa8b4efb13fac08c8bfec64b5a15dffcc460ce05
David Deveaux <david.dancingelephant@gmail.com>
2025-11-22 19:15:51 +0700

    Add basename to BrowserRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';import Home from './pages/Home';export default function App(){return(<BrowserRouter><Routes><Route path='/' element={<Home/>}/></Routes></BrowserRouter>)}
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+export default function App() {
+  return (
+    <BrowserRouter basename="/NAVA-PEACE-SITE">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
